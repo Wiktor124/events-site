17ff1c28dcc8fab461381422180269d4bf6045ad
Víctor Peña <victortkd88@gmail.com>
2023-06-16 11:17:32 -0600

    auto-height-node.js added and some files updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import { initTabs } from "./home/tabs.js";
+import initTabs from "./home/tabs.js";
+import autoHeight from "./utils/auto-height-node.js";
 
 window.onload = () => {
   initTabs();
+  autoHeight();
 }
 
---
+++ js/utils/auto-height-node.js
@@ -0,0 +1,7 @@
+function autoHeight       () {
+  const headerHeight = document.querySelector('#header').offsetHeight;
+  const mainContainer = document.querySelector('#main-gallery');
+  mainContainer.style.height = `calc(100vh - ${headerHeight}px)`;
+  mainContainer.style.overflow = 'auto';
+}
+export default autoHeight;
